b9b950952de86462fdf655e941452d95230fc653
kitu <kristoftunner95@gmail.com>
2026-08-24 19:48:03 +0200

    working on the orderbook

--- src/orderbook.rs
+++ src/orderbook.rs
@@ -1,20 +1,62 @@
-enum Symbol{
+#[derive(Debug)]
+enum Symbol {
     BnbBtc,
-    BnbUsdt
+    BnbUsdt,
 }
 
-struct Order
-{
+#[derive(Debug, Clone)]
+enum OrderType {
+    Buy,
+    Ask,
+}
+
+#[derive(Debug, Clone)]
+struct Order {
+    order_type: OrderType,
     price: i64,
     quantity: i64,
 }
 
-struct OrderBook
-{
+#[derive(Debug)]
+struct OrderBook {
     symbol: Symbol,
     timestamp: u64,
     first_update_id: i64,
     last_update_id: i64,
     bids: Vec<Order>,
     asks: Vec<Order>,
+}
+
+impl OrderBook {
+    fn push_order(&mut self, order: &Order) {
+        match order.order_type {
+            OrderType::Ask => {
+                for (idx, item) in self.asks.iter().enumerate() {
+                    if order.price > item.price {
+                        &mut self.asks.insert(idx, order.clone());
+                        return;
+                    }
+                }
+                &mut self.asks.push(order.clone());
+                
+            }
+            OrderType::Buy => {
+                for (idx, item) in self.bids.iter().enumerate() {
+                    if order.price < item.price {
+                        self.bids.insert(idx, order.clone());
+                        return;
+                    }
+                }
+                &mut self.bids.push(order.clone());
+            }
+        }
+    }
+
+    /// Prints the book content to CLI:
+    /// ORDERS
+    /// <price> /t /t <quantity> \t \t <price>
+    fn print_book(&self)
+    {
+
+    }
 }
